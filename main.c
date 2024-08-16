e04b3e0cbaa40a948bf52f143b15c767c1d051db
sami-001 <samiaboualfadl@gmail.com>
2024-08-16 15:26:07 +0300

    fixes and tweaks

--- main.c
+++ main.c
@@ -8,7 +8,6 @@ char board[3][3];
 
 int x;
 int y;
-int empty_spaces;
 
 const char PLAYER = 'X';
 const char COMPUTER = 'O';
@@ -20,15 +19,22 @@ void newBoard();
 void displayBoard();
 void playAgain();
 
-void checkEmptySpaces();
+int emptySpaces();
 
 void playerPlay();
 void computerPlay();
 
 void checkWinner();
+void checkDraw();
 
-//                                notes:    1. draw does not work
-//                                          2. winning before computer by one turn does not work   
+
+//              notes:
+
+//                   1. draw does not work 
+//                     ^- now works
+
+//                   2. winning before computer by one turn does not work
+//                     ^- should be fixed   
 
 int main()
 {   
@@ -42,29 +48,23 @@ int main()
 
 
     while (winner == ' ') {
+
         playerPlay();
-
-        computerPlay();
-
         checkWinner();
 
-        if (empty_spaces <= 0) {
-            displayBoard();
-            printf("\nIt is a draw\n");
-            playAgain();
-    }
-    
+        computerPlay();    
+        checkWinner();
 
     }
-    /*    if (checkEmptySpaces() == 0) {
-        displayBoard();
-        playAgain();
-    } */
 
+    // if again? (y/n) -> n, in the playAgain(), it will break the cycle. and start here
+    return 0;
 
 }
 
 
+
+// prompts the player to play again
 void playAgain() {
 
     printf("play again? (y/n) ");
@@ -82,14 +82,15 @@ void playAgain() {
             printf("\n");
             break;
         }
+        
 
     }
     
 }
 
 
+// initialises the variables for a new board 
 void newBoard() {
-    empty_spaces = 9;
     winner = ' ';
     
     for (int i = 0; i < 3; i++) {
@@ -98,78 +99,98 @@ void newBoard() {
             board[i][j] = ' ';
         }
     }
+
 }
 
+
+// Renders the board
 void displayBoard() {
 
+    printf("\n");
     printf("   1   2   3 \n");
     printf("1  %c | %c | %c \n", board[0][0], board[1][0], board[2][0]);
-    printf("  ---|---|---\n");
+    printf("  ---+---+---\n");
     printf("2  %c | %c | %c \n", board[0][1], board[1][1], board[2][1]);
-    printf("  ---|---|---\n");
+    printf("  ---+---+---\n");
     printf("3  %c | %c | %c \n", board[0][2], board[1][2], board[2][2]);
+    printf("\n");
     
 }
 
-void checkEmptySpaces() {
+// returns how many empty spaces are on the board
+int emptySpaces() {
+
+    int empty_spaces = 0;
 
     for (int i = 0; i < 3; i++) {
         for (int j = 0; j < 3; j++) {
 
-            if (board[i][j] != ' ') {
-                if (empty_spaces != 0) {
-                    empty_spaces--;
-                }
+            if (board[i][j] == ' ') {
+                empty_spaces++;
             }
         }
     }
+    //printf("\n%i\n", empty_spaces);
+
+    return empty_spaces;
 
 }
 
-
+// prompts the player to play
 void playerPlay() {
     
-    
-    do {
-    printf("Enter column index (1-3): ");
-    scanf("%d", &x);
-    printf("\nEnter row index (1-3): ");
-    scanf("%d", &y);
-    printf("\n");
-    x--;
-    y--;
+    if (emptySpaces() > 0) {
+        
+        do {
+        printf("Enter column index (1-3): ");
+        scanf("%d", &x);
+        printf("\nEnter row index (1-3): ");
+        scanf("%d", &y);
+        x--;
+        y--;
 
-    if (board[x][y] != ' ') {
-        displayBoard();
-        printf("Can't play here.\n");
-        continue;
+        if (board[x][y] != ' ' || 1 > (x + 1) || (x + 1) > 3  || 1 > (y + 1) || (y + 1) > 3) 
+        {
+            displayBoard();
+            printf("Can't play here.\n");
+            continue;
+        }
+
+        } while (board[x][y] != ' ' || 1 > (x + 1) || (x + 1) > 3  || 1 > (y + 1) || (y + 1) > 3);
+
+
+        board[x][y] = PLAYER;
+        //displayBoard();
     }
 
-    } while (board[x][y] != ' ');
-    
-
-    board[x][y] = PLAYER;
 }
 
 
-
+// lets the computer play
 void computerPlay() {
+
+    if (emptySpaces() > 0 && winner == ' ') {
     
-    int cx;
-    int cy;
+        int cx;
+        int cy;
 
 
-    do {
-        cx = rand() % 3;
-        cy = rand() % 3;
+        do {
+            cx = rand() % 3;
+            cy = rand() % 3;
 
-    } while (board[cx][cy] != ' ');
+        } while (board[cx][cy] != ' ');
 
-    board[cx][cy] = COMPUTER;
+        board[cx][cy] = COMPUTER;
 
-    displayBoard();
+
+        displayBoard();
+
+    } 
+
 }
 
+// checks if a there is a win or a draw
 void checkWinner() {
 
     for (int i = 0; i < 3; i++) {
@@ -203,12 +224,23 @@ void checkWinner() {
         }
     }
 
+
     if (winner != ' ') {
+        displayBoard();
         printf("\nThe winner is %c\n", winner);
         playAgain();
     }
 
-
+    checkDraw();
     
 
+}
+
+void checkDraw() 
+{
+    if (emptySpaces() <= 0 && winner == ' ') {
+        displayBoard();
+        printf("\nIt is a draw\n");
+        playAgain();
+    }
 }
